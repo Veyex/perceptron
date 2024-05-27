a9a45f2bc8dc634699223852a35f2f1e33ffccf4
Veyex <79301840+Veyex@users.noreply.github.com>
2024-05-27 10:32:43 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace perceptron
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            //setup
+            Random RNG = new Random();
+            double[] inputs0 = new double[] { 1, 1, 1, 1 };
+            double[] inputs1 = new double[] { 0, 1, 0, 1 };
+            double[] inputs2 = new double[] { 0, 0, 1, 1 };
+            List<Double[]> inputs = new List<Double[]> { inputs0, inputs1, inputs2};
+
+            double[] tarOutputs = new double[] { 0, 0, 0, 1 };
+
+            double[] weights = new double[] { RNG.NextDouble(), RNG.NextDouble(), RNG.NextDouble() };
+
+            //learning stage
+            double learnRate = 1;
+            double[] calcOutputs = new double[4];
+            for (int n = 0; n < 100; n++)
+            {
+                for (int i = 0; i < inputs1.Length; i++)
+                {
+                    calcOutputs[i] = Sigmoid((inputs0[i] * weights[0]) + (inputs1[i] * weights[1]) + (inputs2[i] * weights[2]));
+                    for (int j = 0; j < weights.Length; j++)
+                    {
+                        weights[j] += learnRate * (tarOutputs[i] - calcOutputs[i]) * inputs[j][i];
+                    }
+                }
+            }
+
+            //print final outputs
+            foreach (double a in calcOutputs)
+            {
+                Console.WriteLine(a);
+            }
+        }
+
+        //sigmoid function
+        static double Sigmoid(double x)
+        {
+            return 1 / (1 + Math.Exp(-x));
+        }
+    }
+}
